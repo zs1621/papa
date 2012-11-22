8cdd5d0ba5fe6fcc18ac875e7a57def8c6161ef6
root <root@zs-VirtualBox.(none)>
2012-11-22 17:39:41 +0800

    add papa.py modify README.md

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ papa
 ====
 
 papa user show
+
+1 `papa.py`  retrive the papa user data ,and save to mysql
+
+2  webpy frame to show the data(use pubuliu)
--- papa.py
+++ papa.py
@@ -1,58 +1,50 @@
-import web
-import os
-import model
+#!/usr/bin/env python
+#-*-coding="utf-8"-*-
+import MySQLdb
+import httplib
 import simplejson
-### Templates
-
-web.config.debug = True
-
-urls =(
-        "/", "Index",
-        "/test","test",
-        "/like","action",
-)
-
-app = web.application(urls,globals())
-
-render = web.template.render('templates',base='base')
-
-class test:
-    def GET(self):
-        receive = web.input();
-        if receive:
-            user = model.get_user(receive.p)
-            data = simplejson.dumps(user.list())
-            print len(user)
-            return data
+import time
+import MySQLdb.cursors
+con = MySQLdb.connect(host='localhost',user='****',passwd='*****',db='pa',charset='utf8')
+cursor = con.cursor(cursorclass=MySQLdb.cursors.DictCursor)
+#cursor.execute("create table user(avatar varchar(80) ,id char(8) unique,follower int,following int, name varchar(20), createtime bigint,background varchar(80),signature varchar(100) )")
+#5731 12256
+def papa(url):
+    i,j = 1,249
+    for n in range(1,1000000):
+        pa = httplib.HTTPConnection(url)
+        pa.request('GET','/api/v1/timeline/recommend?count=50&cursor='+str(j)) 
+        result = pa.getresponse()
+        print result
+        if not result:
+            print 'fuck'
+        content = simplejson.loads(result.read())
+        response = content['response']
+        startime = time.clock()
+        if j == 249:
+            last = response[len(response)-1]['post']['id']
+            startime = time.clock()
+            for m in range(0,len(response)): 
+                for a in response[m]['post']['favors']:
+                    signature = a['user']['signature']
+                    print a['user']['name']
+                    user =[a['user']['avatarLarge'][0:-7], a['user']['id'],a['user']['followerCount'],a['user']['followingCount'],a['user']['name'],a['user']['registerTime'],a['user']['background'][0:-7],signature]
+                    cursor.execute("replace into user values(%s,%s,%s,%s,%s,%s,%s,%s)",user)
+            endtime = time.clock()
+            con.commit()
+            print endtime-startime
         else:
-            return "error request"
-class Index:
-    """Show page"""
-    def GET(self):
-        print web.ctx.method
-        users = model.get_users()
-        return render.index(users)
-class action:
-    def GET(self):
-        print web.ctx.method
-        id = web.input().id
-        print id
-        model.update_user(id)
-        o = dict(r = 0) 
-        return simplejson.dumps(o)
-
-    def DELETE(self):
-        print web.ctx.method
-        id = web.data().id
-        print id
-        model.delete_user(id)
-        o = dict(r = 0)
-        return simplejson.dumps(o)
-
-#application = web.application(urls, globals()).wsgifunc()
-#curdir = os.path.dirname(__file__) #session =
-#web.session.Session(app,web.session.DiskStore(os.path.join(curdir,'sessions')),)
-#application = app.wsgifunc()
-#
-if __name__ == '__main__':
-    app.run()
+            if last != response[len(response)-1]['post']['id']:
+                #todo upsert into the first
+                for a in response[0]['post']['favors']:
+                    signature = a['user']['signature']
+                    user =[a['user']['avatarLarge'][0:-7], a['user']['id'],a['user']['followerCount'],a['user']['followingCount'],a['user']['name'],a['user']['registerTime'],a['user']['background'][0:-7],signature]
+                    cursor.execute("replace into user values(%s,%s,%s,%s,%s,%s,%s,%s)",user)
+                last = response[len(response)-1]['post']['id']
+                print j
+        con.commit()
+        time.sleep(1)
+        j=j+1
+if __name__ == "__main__":
+    url = "api.papa.me"
+    papa(url)
